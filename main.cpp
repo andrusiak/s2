a828f95e9253fd20efb2b64ca16da3b40ea8b41b
Yurii Andrusiak <yuriy.andrusyak@gmail.com>
2013-05-20 02:01:51 +0300

    develop iteration algorithm using simple fitting

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "matrix.h"
 
 #define NMAX 10000
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
@@ -24,14 +25,63 @@ struct AutocorrFunction{
 	long frames_number;
 };
 
+struct SimpleModelState{
+	float s2;
+	float tau_s;
+};
+
+struct ExtendedModelState{
+	float s2;
+	float sf2;
+	float tau_f;
+	float tau_s;
+};
+
 void printACF(float * t, float * C1, int n){
     for(int i = 0; i<n; i++){
      printf("time = %5.1f , C1 = %f\n", *(t+i),*(C1+i));
     }
 }
 
-void calcSimpleApproad(AutocorrFunction* acf, float& s2, float& tau_s){
+SimpleModelState doIteration(AutocorrFunction* acf, SimpleModelState initialState) {
+	/**
+	 * Use Newton's method for optimization
+	 *
+	 * X[t+1]=X[t] - H^-1 * g
+	 * 		where H(s2,ts) - hessian of f(x,y)= (1 - s2) Exp[-ti/ts] + s2 in point(s2, ts)
+	 */
 
+	float s2 = initialState.s2, tau_s = initialState.tau_s;
+
+	Matrix hessian(2, 2, "H");
+	Matrix gradient(2, 1, "grad");
+
+	for (long i = 0; i < acf->frames_number; i++) {
+
+	float ti=*(acf)->t, Ci = *(acf)->C1;
+	hessian(0,0) += 0;
+	hessian(0,1) += -(ti*exp(-ti/tau_s))/(tau_s*tau_s);
+	hessian(1,0) += -(ti* exp(-ti/tau_s))/(tau_s*tau_s);
+	hessian(1,1) += -(ti*(s2-1)*(ti-2*tau_s)*exp(-ti/tau_s))/(tau_s*tau_s*tau_s*tau_s);
+
+	gradient(0,0) += 2.0f*(exp(-ti/tau_s)-1)*(Ci + exp(-ti/tau_s)*(s2 - 1) - s2); // 1 - exp(-ti / tau_s);
+	gradient(1,0) += 2.0f*exp(-2*ti/tau_s)*(s2 - 1)*ti*(-1 + exp(ti/tau_s)*(Ci - s2) + s2)/(tau_s*tau_s); //-(ti*(s2-1)*exp(-ti/tau_s))/(tau_s*tau_s);
+	}
+	return initialState;
+}
+
+SimpleModelState calcSimpleApproad(AutocorrFunction* acf, SimpleModelState* initialValue) {
+	float eps = 0.0001;
+//	float s2i = 0.5, s2next = s2i + 10 * eps;
+//	float tau_s = 50.0;
+	SimpleModelState currentValue = *initialValue;
+	do {
+		*initialValue = currentValue;
+
+		currentValue = doIteration(acf, *initialValue);
+	} while (fabs(currentValue.s2 - initialValue->s2) > eps);
+
+	return currentValue;
 }
 
 void calcExtendedApproad(float* t, float* C1, float& s2, float& sf2, float& tau_f, float& tau_s){
@@ -88,11 +138,19 @@ int main(void) {
                     i = 0;
                     // printACF(t, C1, nFrames);
                     s2 = processACF(t, C1, nFrames);
-                    AutocorrFunction acf = {t1, c2, nFrames};
-                    float tau_s;
-                    calcSimpleApproad(&acf, s2, tau_s);
+
+                    AutocorrFunction acf;
+                    acf.t=t;
+                    acf.C1 = C1;
+                    acf.frames_number = nFrames;
+
+                    SimpleModelState* state;
+					state->s2 = 0.5;
+					state->tau_s = 50;
+                    calcSimpleApproad(&acf, state);
+
                     ++nResidue;
-                    fprintf(output,"%d \t%f \n", nResidue, s2);
+                    fprintf(output,"%d \t%f \t%f \n", nResidue, state->s2, state->tau_s);
                   //  exit(1);
                 }else{
                     t[i] = time_; C1[i] = C1_; i++;
